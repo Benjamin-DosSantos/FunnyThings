c1806af8760c762769d06863fbd1d6717c498f07
Benjamin DosSantos <BlackPenSoftware@Gmail.com>
2016-03-17 16:46:57 -0400

    First push of old projects to GitHub

---
+++ src/Printing.java
@@ -0,0 +1,14 @@
+
+public class Printing {
+	public static void main(String[] args){
+		print("Hello");
+	}// End of main
+	
+	public static void print(String input){
+		System.out.print(input);
+	}// End of print
+	
+	public static void println(String input){
+		System.out.println(input);
+	}// End of println
+}// End of class
